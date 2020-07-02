b7120af18a3566066e7f2cef8f0b1674e4fd2c8b
Kamyar <kammoh@gmail.com>
2020-07-02 01:02:42 -0400

    print DC variable only in synth-dc target

--- hardware/lwc_dc.mk
+++ hardware/lwc_dc.mk
@@ -10,24 +10,15 @@ DC_START_TCL=$(SYNTH_FRAMEWORK_ROOT)/tools/dc/dc-start.tcl
 # see hardware/scripts/synth/tools/dc/compile-options.tcl
 # .EXPORT_ALL_VARIABLES:
 export ADK_DIR ?= $(SYNTH_FRAMEWORK_ROOT)/adk/freepdk-45nm/
-$(info ADK_DIR=$(ADK_DIR))
 export CLOCK_PERIOD ?= 2.0
-$(info Target frequency: $(shell echo 1000.0/${CLOCK_PERIOD} | bc) MHz)
 export HIGH_EFFORT_AREA_OPT ?= True
-$(info HIGH_EFFORT_AREA_OPT=$(HIGH_EFFORT_AREA_OPT))
 export FLATTEN_EFFORT ?= 3
-$(info FLATTEN_EFFORT=$(FLATTEN_EFFORT))
 export GATE_CLOCK ?= True
-$(info GATE_CLOCK=$(GATE_CLOCK))
 
 DC_LOGS_DIR ?= logs
-$(info Logs will be written to "$(DC_RESULTS_DIR)")
 export DC_RESULTS_DIR ?= $(CORE_ROOT)/results
-$(info Synthesis artefacts will be stored in "$(DC_RESULTS_DIR)")
 export DC_REPORTS_DIR ?= $(CORE_ROOT)/reports
-$(info Reports will be stored in "$(DC_REPORTS_DIR)")
 export DC_ALIB_DIR ?= $(CORE_ROOT)/alib
-$(info ALIB directory is "$(DC_ALIB_DIR)")
 
 ########################################
 
@@ -47,6 +38,15 @@ $(DC_REPORTS_DIR):
 	mkdir -p $@
 
 synth-dc: $(DC_LOGS_DIR) $(DC_RESULTS_DIR) $(DC_REPORTS_DIR) $(VERILOG_FILES) $(VHDL_FILES)
+	$(info ADK_DIR=$(ADK_DIR))
+	$(info Target frequency: $(shell echo 1000.0/${CLOCK_PERIOD} | bc) MHz)
+	$(info HIGH_EFFORT_AREA_OPT=$(HIGH_EFFORT_AREA_OPT))
+	$(info FLATTEN_EFFORT=$(FLATTEN_EFFORT))
+	$(info GATE_CLOCK=$(GATE_CLOCK))
+	$(info Logs will be written to "$(DC_RESULTS_DIR)")
+	$(info Synthesis artefacts will be stored in "$(DC_RESULTS_DIR)")
+	$(info Reports will be stored in "$(DC_REPORTS_DIR)")
+	$(info ALIB directory is "$(DC_ALIB_DIR)")
 	$(DC_CMD) -f $(DC_START_TCL) -output_log_file $(DC_LOGS_DIR)/dc.log
 
 clean-dc:
