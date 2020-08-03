547430dfa4f5fc88fe0245a10ecf92b4667b11de
Rich H <rhaeussl@mason.gmu.edu>
2020-08-03 19:19:54 -0400

    Please keep W  32 the default

--- hardware/LWCsrc/NIST_LWAPI_pkg.vhd
+++ hardware/LWCsrc/NIST_LWAPI_pkg.vhd
@@ -28,8 +28,8 @@ use IEEE.STD_LOGIC_1164.all;
 package NIST_LWAPI_pkg is
 
     --! External bus: supported values are 8, 16 and 32 bits
-    constant W       : integer := 8;
-    constant SW      : integer :=W;
+    constant W       : integer := 32;
+    constant SW      : integer := W;
     
     --! asynchronous and active-low reset (for ASIC targets)
     constant ASYNC_RSTN    : boolean := False;
