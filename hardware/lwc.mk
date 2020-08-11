4eb033026debf6ddd09d13c08bcb450b8c684964
Kamyar Mohajerani <kammoh@gmail.com>
2020-08-11 09:41:58 -0400

    check make version

--- hardware/lwc.mk
+++ hardware/lwc.mk
@@ -2,6 +2,17 @@ ifndef LWC_ROOT
 $(error LWC_ROOT must be defined in design-specific Makefile)
 endif
 
+MIN_MAKE_VERSION := 4.1
+MAKE_VERSION_OK := $(filter $(MIN_MAKE_VERSION),$(firstword $(sort $(MAKE_VERSION) $(MIN_MAKE_VERSION))))
+
+ifeq ($(MAKE_VERSION),)
+$(error MAKE_VERSION was not supported. Use GNU make >= $(MIN_MAKE_VERSION))
+endif
+
+ifeq ($(MAKE_VERSION_OK),)
+$(error Make version is not supported! Use GNU make >= $(MIN_MAKE_VERSION))
+endif
+
 LWC_ROOT := $(realpath $(LWC_ROOT))
 HW_DIR := $(LWC_ROOT)/hardware
 
