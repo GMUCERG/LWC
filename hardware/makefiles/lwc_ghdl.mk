40b9fb54babd4715193cdf85a5aa093f36e0ce92
Kamyar Mohajerani <kammoh@gmail.com>
2020-08-11 14:39:30 -0400

    Fix #22

--- hardware/makefiles/lwc_ghdl.mk
+++ hardware/makefiles/lwc_ghdl.mk
@@ -4,9 +4,8 @@ GHDL_BIN ?= ghdl
 
 GHDL_OPTIMIZE ?= -O3
 
-GHDL_OPT = -frelaxed-rules --warn-no-vital-generic -frelaxed $(GHDL_OPTIMIZE)
 GHDL_WARNS ?= -Wbinding -Wreserved -Wlibrary -Wvital-generic -Wdelayed-checks -Wbody -Wspecs -Wunused --warn-no-runtime-error
-GHDL_ELAB_OPTS ?= --mb-comments 
+GHDL_ANALYSIS_OPTS ?= -frelaxed-rules --warn-no-vital-generic -frelaxed $(GHDL_OPTIMIZE) --mb-comments 
 GHDL_SIM_OPTS ?=
 
 ifneq ($(strip $(VCD_FILE)),)
@@ -20,12 +19,7 @@ GHDL_SIM_OPTS += --wave=$(GHW_FILE)
 endif
 
 
-### GHDL analyse
-$(WORK_LIB)-obj$(VHDL_STD).cf: $(SIM_VHDL_FILES) $(FORCE_REBUILD) config-vars
-ifneq ($(strip $(VHDL_FILES)),)
-	@rm -f $@ 
-	$(GHDL_BIN) -a $(GHDL_OPT) $(GHDL_WARNS) $(SIM_VHDL_FILES)
-endif
+
 
 SIM_ONLY_VHDL_FILES := $(VHDL_ADDITIONS) $(LWC_TB) 
 SIM_VHDL_FILES = $(VHDL_FILES) $(SIM_ONLY_VHDL_FILES)
@@ -34,19 +28,26 @@ GHDL_GENERICS_OPTS=$(shell $(PYTHON3_BIN) $(LWC_ROOT)/hardware/scripts/config_pa
 
 GHDL_STD_OPT = --std=$(if $(filter $(VHDL_STD),93),93c,$(VHDL_STD))
 
-GHDL_OPT += $(GHDL_STD_OPT)
+GHDL_ANALYSIS_OPTS += $(GHDL_STD_OPT)
 
 ### GHDL analyze testbench files, elaborate, and run
 .PHONY: sim-ghdl help-ghdl clean-ghdl
 
+### GHDL analyse
+$(WORK_LIB)-obj$(VHDL_STD).cf: $(SIM_VHDL_FILES) $(FORCE_REBUILD) config-vars
+ifneq ($(strip $(VHDL_FILES)),)
+	@rm -f $@ 
+	$(GHDL_BIN) -a $(GHDL_ANALYSIS_OPTS) $(GHDL_WARNS) $(SIM_VHDL_FILES)
+endif
+
 sim-ghdl: $(WORK_LIB)-obj$(VHDL_STD).cf config-vars
-	$(GHDL_BIN) -e $(GHDL_OPT) $(GHDL_WARNS) $(GHDL_ELAB_OPTS) $(SIM_TOP) 
-	$(GHDL_BIN) -r $(SIM_TOP) $(GHDL_SIM_OPTS) $(GHDL_GENERICS_OPTS) $(VCD_OPT)
+	# $(GHDL_BIN) -e $(GHDL_ANALYSIS_OPTS) $(GHDL_WARNS) $(SIM_TOP) 
+	$(GHDL_BIN) --elab-run $(GHDL_STD_OPT) $(SIM_TOP) $(GHDL_SIM_OPTS) $(GHDL_GENERICS_OPTS) $(VCD_OPT)
 
 ifeq ($(strip $(VHDL_FILES)),)
 YOSYS_READ_VHDL_CMD := 
 else
-YOSYS_READ_VHDL_CMD := ghdl $(GHDL_OPT) $(GHDL_WARNS) $(TOP);
+YOSYS_READ_VHDL_CMD := ghdl $(GHDL_ANALYSIS_OPTS) $(GHDL_WARNS) $(TOP);
 endif
 
 help-ghdl:
