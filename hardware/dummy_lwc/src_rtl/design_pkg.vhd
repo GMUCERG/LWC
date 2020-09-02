3eee8ced0d67bff4c77724fa9ae89c4ea19df690
Kamyar Mohajerani <kammoh@gmail.com>
2020-09-02 00:04:25 -0400

    remove more artifacts mistakingly commited
    ... by other devs

--- hardware/dummy_lwc/src_rtl/design_pkg.vhd
+++ hardware/dummy_lwc/src_rtl/design_pkg.vhd
@@ -29,10 +29,6 @@ package design_pkg is
     -- Range definition must not change.
     constant AD_CNT_WIDTH    : integer range 4 to 64 := 32;  --! Width of AD Bit counter
     constant MSG_CNT_WIDTH   : integer range 4 to 64 := 32;  --! Width of MSG (PT/CT) Bit counter
-    
-    --! Asynchronous and active-low reset.
-    --! Can be set to `True` when targeting ASICs given that your CryptoCore supports it.
-    constant ASYNC_RSTN      : boolean := false;
 
 --------------------------------------------------------------------------------
 ------------------------- DO NOT CHANGE ANYTHING BELOW -------------------------
